edb1ba186e7c2d98032a9c0f7b59d00dfa339222
_TItto_ <68274635+TittoBeast@users.noreply.github.com>
2022-03-02 21:28:00 +0700

    Add files via upload

---
+++ 8.3.py
@@ -0,0 +1,27 @@
+a=input("Nhập ngày: ")
+from time import sleep
+if a=="8/3":
+
+    print("###############################################################")
+    print("#               .                                              #");sleep(0.25)
+    print("#          .                                              #");sleep(0.25)
+    print("#       .                                                 #");sleep(0.25)
+    print("#     .                                                   #");sleep(0.25)
+    print("#    .                                                     #");sleep(0.25)
+    print("#   .                                                         #");sleep(0.25)
+    print("#   .                     .                                     #");sleep(0.25)
+    print("#   .                                                          #");sleep(0.25)
+    print("#    .                     .                                    #");sleep(0.25)
+    print("#      .                 .                                      #");sleep(0.25)
+    print("#        .                                                     #");sleep(0.25)
+    print("#           .          .                                        #");sleep(0.25)
+    print("#             .   .                                             #");sleep(0.25)
+    print("#                                                             #");sleep(0.25)
+    print("#                                                             #");sleep(0.25)
+    print("#                                                             #");sleep(0.25)
+    print("#                                                             #");sleep(0.25)
+    print("#                                                             #");sleep(0.25)
+    print("###############################################################")
+
+    import turtle
+   
